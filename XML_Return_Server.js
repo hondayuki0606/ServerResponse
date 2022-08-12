c99f18d5c6df098cb91121fd8db53d3a517cb1ce
hondayuki0606 <55884500+hondayuki0606@users.noreply.github.com>
2022-08-12 16:01:44 +0900

    Update XML_Return_Server.js

--- XML_Return_Server.js
+++ XML_Return_Server.js
@@ -1,22 +1,3 @@
-//var http = require('http');
-//var server = http.createServer();
-//
-//var fs = require('fs');
-//var xml_string;
-//
-//server.on('request', function (req, res)) {
-//    if (req.url.indexOf('TestAPI') != -1) {
-//        fs.readFile('./routeSearch/stub_200_routesearch_response_body_10.xml', 'utf-8', err, files) => {
-//            if (err) {throw err;}
-//            xml_string = files;
-//            res.writeHead(200,{'Content-Type':'application/xml'});
-//            res.end(xml_string);
-//        });
-//    }
-//});
-//
-//server.listen(3000);
-
 // Your code here!
 
 var http = require('http');
@@ -27,7 +8,7 @@ var xml_string;
 
 server.on('request', function (req, res) {
     if (req.url.indexOf('TestAPI') != -1) {
-        fs.readFile('./routeSearch/stub_200_routesearch_response_body_10.xml', 'utf-8', (err, files) => {
+        fs.readFile('./folderSearch/stub_200_return.xml', 'utf-8', (err, files) => {
             if (err) {throw err;}
             xml_string = files;
             res.writeHead(200,{'Content-Type':'application/xml'});
