e92706e799df15d99cfa55bbed3dbfca4bfa17d5
hondayuki0606 <55884500+hondayuki0606@users.noreply.github.com>
2022-08-12 16:00:07 +0900

    add

---
+++ XML_Return_Server.js
@@ -0,0 +1,39 @@
+//var http = require('http');
+//var server = http.createServer();
+//
+//var fs = require('fs');
+//var xml_string;
+//
+//server.on('request', function (req, res)) {
+//    if (req.url.indexOf('TestAPI') != -1) {
+//        fs.readFile('./routeSearch/stub_200_routesearch_response_body_10.xml', 'utf-8', err, files) => {
+//            if (err) {throw err;}
+//            xml_string = files;
+//            res.writeHead(200,{'Content-Type':'application/xml'});
+//            res.end(xml_string);
+//        });
+//    }
+//});
+//
+//server.listen(3000);
+
+// Your code here!
+
+var http = require('http');
+var server = http.createServer();
+
+var fs = require('fs');
+var xml_string;
+
+server.on('request', function (req, res) {
+    if (req.url.indexOf('TestAPI') != -1) {
+        fs.readFile('./routeSearch/stub_200_routesearch_response_body_10.xml', 'utf-8', (err, files) => {
+            if (err) {throw err;}
+            xml_string = files;
+            res.writeHead(200,{'Content-Type':'application/xml'});
+            res.end(xml_string);
+        });
+    }
+});
+
+server.listen(3000);
